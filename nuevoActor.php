ff8c53ab1ed746bfc6cdce28da8071d357938ed1
JaredPV <jaredpv99@micorreo.upp.edu.mx>
2022-07-01 19:16:49 -0500

    Archivos php de formularios de actores

---
+++ nuevoActor.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+	    <title>Nuevo Actor</title>
+    </head>
+    <body>
+        <h1>Agregar Nuevo Actor</h1>
+    </body>
+</html>
