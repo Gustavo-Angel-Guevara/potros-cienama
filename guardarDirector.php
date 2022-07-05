5a3bba4f696f82844eba2a7ca5264ff6a02fc077
LAG17-xd <78927626+LAG17-xd@users.noreply.github.com>
2022-07-05 16:22:32 -0500

    Revert "Revert "directores""
    This reverts commit defbe4e35af796c74c20b815eb5d284b01cb09a4.

---
+++ guardarDirector.php
@@ -0,0 +1,34 @@
+<?PHP
+
+$nombre = $_POST["nombre"];   
+$pais = $_POST["pais"];   
+$fecha = date("fecha");
+$band = $_POST["band"];
+
+  
+$conexion = new mysqli("localhost:8080","root","","peliculas");
+$_GRABAR_SQL = "INSERT INTO $director (idDirector,nombreDirector,fechaNacimiento,pais) VALUES (null,'$nombre','$fecha','$pais')";   
+ 
+$ejecutar = mysql_query($_GRABAR_SQL,$conexion);
+
+
+ if($ejecutar){
+			$res = [
+				"err" => false,
+				"message" => "Registro guardado con Éxito"
+			];
+		}else{
+			$res = [
+				"err" => true,
+				"message" => "Error al guardar los datos intente más tarde"
+			];
+		}
+
+		echo json_encode($res);
+		//-----------------------------------------------------
+	
+
+
+		
+
+?>
---
+++ nuevoDirectores.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nuevo Director</title>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/spinkit.css"><!--IMPORTANT-->
+	<link rel="stylesheet" type="text/css" href="css/estiloCine1.css">
+</head>
+<body>
+<div id="general" align="center">
+
+	<div class="container centrado" align="center">
+		<h2 class="letraBlanca">Agregar nuevos Directores</h2>
+
+		<form data-form='true' action="guardarDirector.php" method="POST">
+			<div class="row">
+
+				<div class="col-lg-12">
+					<br>
+				</div>
+
+				<div class="col-lg-8">
+					<input data-validation='required maxLength-150' class="form-control" name="nombre" type="text" placeholder="Nombre del director">
+				</div>
+
+				<div class="col-lg-12">
+					<br>
+				</div>
+				<div class="col-lg-4">
+					<input data-validation='required' class="form-control" type="date" name="fecha">		
+				</div>
+
+				<div class="col-lg-12">
+					<br>
+				</div>
+
+				<div class="col-lg-6">
+					<input data-validation='required only-words maxLength-30' class="form-control" type="text" name="pais" placeholder="Pais">
+				
+
+				<div class="col-lg-12">
+					<br>
+				</div>
+
+				<div class="col-lg-10" data-warn> <!--Data-warn sirve como referencia para insertar un mensaje al validar los campos-->
+					
+				</div>
+
+				<div class="col-lg-2">
+					<input type="submit" class="btn btn-primary" value="Guardar" >
+					
+				</div>
+
+
+			</div>
+		</div>	
+	</form>
+
+		<!--Animación de cargando para confirmación de nuevo registro-->
+		<div class="loader sk-flow mx-auto d-none">
+			<div class="sk-flow-dot"></div>
+			<div class="sk-flow-dot"></div>
+			<div class="sk-flow-dot"></div>
+		</div>
+	</div>
+	<script src='./js/index.js' type='module'></script> <!--IMPORTANT-->
+</body>
+</html>
