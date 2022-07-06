c1ab2a23022181eb63664f8fc9bdc4ff489dc6dd
YoabUPP <kevin02lopez02@micorreo.upp.edu.mx>
2022-07-06 15:44:53 -0500

    Agregue la pagina error 404 y un apartado para tener las conexiones

---
+++ 404.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-sacle=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/404.css">
+	<style>
+	</style>
+	</head>
+	<body>
+		<h1>ERROR 404</h1>
+		<h3>La ubicacion que buscas no existe :(</h3>
+		
+		<div>
+			<a href="index.php" class='btn'>Regresar a la pagina de inicio</a>
+		</div>
+		
+		</body>
+		</html>
